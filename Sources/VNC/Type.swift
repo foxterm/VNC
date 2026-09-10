b3132e4d86e8c372acbe07153bac19c8eb5c1f1d
FoxTerm <foxterm@foxmail.com>
2026-09-11 05:30:07 +0800

    Update Type.swift

--- Sources/VNC/Type.swift
+++ Sources/VNC/Type.swift
@@ -4,9 +4,16 @@
 
 import Foundation
 
+/// VNC 图像颜色深度枚举
 public enum ColorDepth: String, CaseIterable {
-    case bit32, bit16, bit8
+    /// 32位真彩色 (24位色彩 + 8位保留/ Alpha)
+    case bit32
+    /// 16位高彩色 (通常为 RGB565 / RGB555)
+    case bit16
+    /// 8位伪彩色 / 256色
+    case bit8
 
+    /// 对应的位深度数值 (Int32)，方便直接传给底层 C 库
     var bit: Int32 {
         switch self {
         case .bit32:
