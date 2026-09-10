cd5b2513d163e3802c6b41f66c915bcba7fa8a53
FoxTerm <foxterm@foxmail.com>
2026-09-11 07:19:22 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -31,6 +31,7 @@ let package = Package(
             ],
             linkerSettings: [
                 .linkedLibrary("z"),
+                .linkedLibrary("sasl2"),
             ]
         ),
         .binaryTarget(
