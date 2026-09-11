52869451c22f0e98c73f1dfd42ebbcc7246dae35
FoxTerm <foxterm@foxmail.com>
2026-09-11 21:13:12 +0800

    Update VNCImageView.swift

--- Sources/VNC/Image/VNCImageView.swift
+++ Sources/VNC/Image/VNCImageView.swift
@@ -271,6 +271,16 @@ public class VNCEventHandlingView: PlatformView {
             addTrackingArea(trackingArea)
         }
 
+        override func mouseEntered(with event: NSEvent) {
+            super.mouseEntered(with: event)
+            NSCursor.hide()
+        }
+
+        override func mouseExited(with event: NSEvent) {
+            super.mouseExited(with: event)
+            NSCursor.unhide()
+        }
+
         override func mouseMoved(with event: NSEvent) {
             processMouseEvent(event)
         }
