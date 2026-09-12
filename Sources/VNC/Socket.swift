c401151a3bf74806d697143b5a07d774d38ea721
FoxTerm <foxterm@foxmail.com>
2026-09-12 13:32:34 +0800

    Update Socket.swift

--- Sources/VNC/Socket.swift
+++ Sources/VNC/Socket.swift
@@ -52,16 +52,17 @@ public extension VNC {
     }
 
     /// 当前 Socket 的网络流量统计
-    /// - Returns: 元组 (send: 已发送字节数, recv: 已接收字节数)
-    var trafficStats: (send: UInt64, recv: UInt64) {
-        guard fd >= 0 else { return (0, 0) }
+    /// - Returns: 元组 (send: 已发送字节数, recv: 已接收字节数, rtt: 实时往返时间（微秒))
+    var trafficStats: (send: UInt64, recv: UInt64, rtt: UInt32) {
+        guard fd >= 0 else { return (0, 0, 0) }
         var stats = FdTrafficStats()
         guard etos_socket_get_traffic_stats(fd, &stats) == 0 else {
-            return (0, 0)
+            return (0, 0, 0)
         }
         let tx = etos_stats_get_tx(&stats)
         let rx = etos_stats_get_rx(&stats)
-        return (tx, rx)
+        let rtt = etos_stats_get_rtt(&stats)
+        return (tx, rx, rtt)
     }
 
     /// 启动 Socket 事件轮询监听
