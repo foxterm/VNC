cc72daa23d1a8c7915761549a696a5587d9ab2e5
FoxTerm <foxterm@foxmail.com>
2026-09-12 15:24:59 +0800

    Update build_xcframework.sh

--- build_xcframework.sh
+++ build_xcframework.sh
@@ -289,22 +289,12 @@ prepare_headers() {
 
     # 3. 创建 Umbrella Header
     cat << 'EOF' > "${target_headers_dir}/libvncclient.h"
-#ifndef LIBVNCCLIENT_UMBRELLA_H
-#define LIBVNCCLIENT_UMBRELLA_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-
 #include "rfb/rfbclient.h"
 #include "rfb/rfbproto.h"
 #include "rfb/rfbregion.h"
 #include "rfb/keysym.h"
 #include "rfb/threading.h"
 #include "rfb/rfbconfig.h"
-
-#endif
 EOF
 
     # 4. 创建 modulemap
